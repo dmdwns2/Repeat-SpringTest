59400947ca33faba66d0ee78e3f99ccb5e5773d7
dmdwns2 <ssreki1@naver.com>
2022-10-25 11:46:13 +0900

    221025 Spring 처음부터 복습하기 Factory까지. Test 동작은 안함

--- src/main/java/dao/DaoFactory.java
+++ src/main/java/dao/DaoFactory.java
@@ -1,5 +1,6 @@
 package dao;
 
+import domain.User;
 import org.springframework.context.annotation.Bean;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
@@ -12,7 +13,11 @@ public class DaoFactory {
         UserDao userDao = new UserDao(connectionMaker);
         return userDao;
     }
-    // H2
+
+/* H2
+    UserDao H2UserDao(){
+        return new UserDao(h2DataSource());
+    }
     @Bean
     DataSource h2DataSource() {
         return new EmbeddedDatabaseBuilder().setType(EmbeddedDatabaseType.H2)
@@ -20,5 +25,6 @@ public class DaoFactory {
                 .addScript("classpath:jdbc/schema.sql")
                 .build();
     }
+*/
 
 }
