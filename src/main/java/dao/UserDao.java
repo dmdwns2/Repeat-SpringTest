22446f52e456d3bfcad9a7c111178e84bc48ba0d
dmdwns2 <ssreki1@naver.com>
2022-10-25 14:33:22 +0900

    221025 Spring 처음부터 복습하기 jdbccontext 클래스에 excuteSQL 추가
    Test 동작은 안함 (H2)

--- src/main/java/dao/UserDao.java
+++ src/main/java/dao/UserDao.java
@@ -5,142 +5,57 @@ import org.springframework.boot.autoconfigure.batch.BatchProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 
 import javax.sql.DataSource;
 import java.sql.*;
+import java.util.List;
 import java.util.Map;
 
 public class UserDao {
 
-    private ConnectionMaker connectionMaker;
+    private JdbcTemplate jdbcTemplate;
 
-    private JdbcContext jdbcContext;
-
-    public void setJdbcContext(JdbcContext jdbcContext) {
-        this.jdbcContext = jdbcContext;
+    public UserDao(DataSource dataSource) {
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
     }
 
-    public UserDao() {
-        connectionMaker = new H2ConnectionMaker();
-    }
+    RowMapper<User> rowMapper = new RowMapper<User>() {
+        @Override
+        public User mapRow(ResultSet rs, int rowNum) throws SQLException {
+            User user = new User(rs.getString("id"),
+                    rs.getString("name"),
+                    rs.getString("password"));
 
-    public UserDao(ConnectionMaker connectionMaker) {
-        this.connectionMaker = connectionMaker;
-    }
-
-    public void jdbcContextWithStatementStrategy (StatementStrategy stmt) throws SQLException{
-        Connection c = null;
-        PreparedStatement ps = null;
-
-        try {
-            c = connectionMaker.makeConnection();
-            StatementStrategy statementStrategy = new DeleteAllStrategy();
-            ps = statementStrategy.makePreparedStatement(c);
-
-            ps.executeUpdate();
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        } finally {
-            if (ps != null) {
-                try {
-                    ps.close();
-                } catch (Exception e) {
-                }
-            }
-            if (c != null) {
-                try {
-                    c.close();
-                } catch (Exception e) {
-                }
-            }
+            return user;
         }
+    };
 
-    }
     public void add(final User user) throws SQLException {
-        this.jdbcContext.workWithStatementStrategy(
-                new StatementStrategy() {
-                    public PreparedStatement makePreparedStatement(Connection c) throws SQLException {
-                        PreparedStatement pstmt = c.prepareStatement("INSERT INTO user(id, name, password) VALUES(?,?,?);");
-                        pstmt.setString(1, user.getId());
-                        pstmt.setString(2, user.getName());
-                        pstmt.setString(3, user.getPassword());
-                        return pstmt;
-                    }
-                }
-        );
+        this.jdbcTemplate.update("insert into users(id, name, password) values (?, ?, ?);",
+                user.getId(), user.getName(), user.getPassword());
     }
+
 
     public User findById(String id) throws SQLException {
-        Map<String, String> env = System.getenv();
-//        Connection c;
-        try {
-            Connection c = connectionMaker.makeConnection();
-
-            // Query문 작성
-            PreparedStatement pstmt = c.prepareStatement("SELECT * FROM users WHERE id = ?");
-            pstmt.setString(1, id);
-
-            // Query문 실행
-            ResultSet rs = pstmt.executeQuery();
-            User user = null;
-            if(rs.next()) {
-                user = new User(rs.getString("id"), rs.getString("name"),
-                        rs.getString("password"));
-            }
-
-
-            rs.close();
-            pstmt.close();
-            c.close();
-            if (user == null)
-                throw new EmptyResultDataAccessException(1); // user null 일때 exception 처리
-            return user;
-
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
+        String sql = "select * from user where id = ?";
+        return this.jdbcTemplate.queryForObject(sql, rowMapper, id);
     }
 
-    public void deleteALl() throws SQLException  {
-        this.jdbcContext.excuteSQL("delete from user");
+    public List<User> getAll() {
+        String sql = "select * from user order by id";
+        return this.jdbcTemplate.query(sql, rowMapper);
+    }
+    public void deleteALl() throws SQLException {
+        this.jdbcTemplate.update("delete from user");
     }
 
     public int getCount() throws SQLException {
-        Connection c = null;
-        PreparedStatement ps = null;
-        ResultSet rs = null;
-        try {
-            c = connectionMaker.makeConnection();
-            ps = c.prepareStatement("select count(*) from user");
-            rs = ps.executeQuery();
-            rs.next();
-            return rs.getInt(1);
-        } catch (SQLException e) {
-            throw e;
-        } finally {
-            if (rs != null) {
-                try {
-                    rs.close();
-                } catch (Exception e) {
-                }
-            }
-            if (ps != null) {
-                try {
-                    ps.close();
-                } catch (Exception e) {
-                }
-            }
-            if (c != null) {
-                try {
-                    c.close();
-                } catch (Exception e) {
-                }
-            }
-        }
+        return this.jdbcTemplate.queryForObject("select count (*) from user;", Integer.class);
     }
-
 
 
     public static void main(String[] args) throws SQLException {
